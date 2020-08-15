5461b69784e554a59c8b6130411a9219687d1c8c
Sreerag p <sreerag33@gmail.com>
2020-08-15 16:54:23 +0530

    chat speech to text

--- menuDrawer/Controllers/chatBotViewController.swift
+++ menuDrawer/Controllers/chatBotViewController.swift
@@ -7,7 +7,9 @@
 //
 
 import UIKit
-
+import Speech
+import AVKit
+import InstantSearchVoiceOverlay
 class chatBotViewController: UIViewController {
     @IBOutlet weak var scroll_view: UIScrollView!
     @IBOutlet weak var inputField: UITextField!
@@ -16,14 +18,55 @@ class chatBotViewController: UIViewController {
     @IBOutlet weak var table_view: UITableView!
     @IBOutlet weak var attachBtn: UIButton!
     @IBOutlet weak var onlinelabel: UILabel!
+    @IBOutlet weak var btnStart: UIButton!
     
+     let voiceOverlayController = VoiceOverlayController()
+    let speechRecognizer        = SFSpeechRecognizer(locale: Locale(identifier: "en-US"))
+
+      var recognitionRequest      : SFSpeechAudioBufferRecognitionRequest?
+      var recognitionTask         : SFSpeechRecognitionTask?
+      let audioEngine             = AVAudioEngine()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         table_view.register(UINib(nibName: "chatTableViewCell", bundle: .main), forCellReuseIdentifier: "cell1")
          table_view.register(UINib(nibName: "chatHeaderTableViewCell", bundle: .main), forCellReuseIdentifier: "header")
+        //btnStart.addTarget(self, action: Selector("holdRelease:"), for: UIControl.Event.touchUpInside);
+        //btnStart.addTarget(self, action: Selector("HoldDown:"), for: UIControl.Event.touchDown)
+        self.setupSpeech()
         setUpDesign()
+       // voiceOverlayController.set
         // Do any additional setup after loading the view.
     }
+    func HoldDown(sender:UIButton)
+    {
+       print("hold down")
+    }
+
+    func holdRelease(sender:UIButton)
+    {
+       print("hold release")
+    }
+    @IBAction func btnStartSpeechToText(_ sender: UIButton) {
+        
+        voiceOverlayController.start(on: self, textHandler: { (text, final,tx)  in
+            self.inputField.text = String(describing: text)
+                  print("voice output: \(String(describing: text))")
+                  print("voice output: is it final? \(String(describing: final))")
+              }, errorHandler: { (error) in
+                  print("voice output: error \(String(describing: error))")
+              })
+         /*  if audioEngine.isRunning {
+               self.audioEngine.stop()
+               self.recognitionRequest?.endAudio()
+               self.btnStart.isEnabled = false
+               self.btnStart.setTitle("Start Recording", for: .normal)
+           } else {
+               self.startRecording()
+               self.btnStart.setTitle("Stop Recording", for: .normal)
+           }*/
+       }
+
     func setUpDesign(){
         self.inputTypeView.layer.cornerRadius = self.inputTypeView.frame.height / 2
         self.inputTypeView.clipsToBounds = true
@@ -36,17 +79,101 @@ class chatBotViewController: UIViewController {
     @IBAction func backBtnPrsd(_ sender: UIButton) {
         self.navigationController?.popViewController(animated: true)
     }
-    
-    /*
-    // MARK: - Navigation
+    func setupSpeech() {
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+           self.btnStart.isEnabled = false
+           self.speechRecognizer?.delegate = self
 
+           SFSpeechRecognizer.requestAuthorization { (authStatus) in
+
+               var isButtonEnabled = false
+
+               switch authStatus {
+               case .authorized:
+                   isButtonEnabled = true
+
+               case .denied:
+                   isButtonEnabled = false
+                   print("User denied access to speech recognition")
+
+               case .restricted:
+                   isButtonEnabled = false
+                   print("Speech recognition restricted on this device")
+
+               case .notDetermined:
+                   isButtonEnabled = false
+                   print("Speech recognition not yet authorized")
+               }
+
+               OperationQueue.main.addOperation() {
+                   self.btnStart.isEnabled = isButtonEnabled
+               }
+           }
+       }
+    func startRecording() {
+
+           // Clear all previous session data and cancel task
+           if recognitionTask != nil {
+               recognitionTask?.cancel()
+               recognitionTask = nil
+           }
+
+           // Create instance of audio session to record voice
+           let audioSession = AVAudioSession.sharedInstance()
+           do {
+               try audioSession.setCategory(AVAudioSession.Category.record, mode: AVAudioSession.Mode.measurement, options: AVAudioSession.CategoryOptions.defaultToSpeaker)
+               try audioSession.setActive(true, options: .notifyOthersOnDeactivation)
+           } catch {
+               print("audioSession properties weren't set because of an error.")
+           }
+
+           self.recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+
+           let inputNode = audioEngine.inputNode
+
+           guard let recognitionRequest = recognitionRequest else {
+               fatalError("Unable to create an SFSpeechAudioBufferRecognitionRequest object")
+           }
+
+           recognitionRequest.shouldReportPartialResults = true
+
+           self.recognitionTask = speechRecognizer?.recognitionTask(with: recognitionRequest, resultHandler: { (result, error) in
+
+               var isFinal = false
+
+               if result != nil {
+
+                   self.inputField.text = result?.bestTranscription.formattedString
+                   isFinal = (result?.isFinal)!
+               }
+
+               if error != nil || isFinal {
+
+                   self.audioEngine.stop()
+                   inputNode.removeTap(onBus: 0)
+
+                   self.recognitionRequest = nil
+                   self.recognitionTask = nil
+
+                   self.btnStart.isEnabled = true
+               }
+           })
+
+           let recordingFormat = inputNode.outputFormat(forBus: 0)
+           inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer, when) in
+               self.recognitionRequest?.append(buffer)
+           }
+
+           self.audioEngine.prepare()
+
+           do {
+               try self.audioEngine.start()
+           } catch {
+               print("audioEngine couldn't start because of an error.")
+           }
+
+           self.inputField.text = "Say something, I'm listening!"
+       }
 }
 extension chatBotViewController:UITableViewDelegate,UITableViewDataSource{
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
@@ -134,5 +261,16 @@ extension UIBezierPath {
 
         path.closeSubpath()
         cgPath = path
+    }
+}
+
+extension chatBotViewController: SFSpeechRecognizerDelegate {
+
+    func speechRecognizer(_ speechRecognizer: SFSpeechRecognizer, availabilityDidChange available: Bool) {
+        if available {
+            self.btnStart.isEnabled = true
+        } else {
+            self.btnStart.isEnabled = false
+        }
     }
 }
